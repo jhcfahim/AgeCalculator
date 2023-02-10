eb8f2f34e2418a0ea485022fdf403105fb3e2c1b
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-10 10:48:26 +0000

    changed the names of the labels to make them improve readibility

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def find_age(d, m, y):
     return age
 
 def display_calc_age(age):
-    t1.config(state='normal')
+    tbox_age.config(state='normal')
     
-    t1.delete('1.0', tk.END)
-    t1.insert(tk.END,age)
-    t1.config(state='disabled')
+    tbox_age.delete('1.0', tk.END)
+    tbox_age.insert(tk.END,age)
+    tbox_age.config(state='disabled')
 
 def validation():
   # gets the three entries
@@ -38,7 +38,7 @@ def validation():
       elif any(ch.isdigit() for ch in y) == False:
         msg = 'year must be a NUMBER'
       else:
-        msg = 'Success!'
+        #msg = 'Success!'
         day = int(d)
         month = int(m)
         year = int(y)
@@ -59,38 +59,38 @@ window.resizable(width=False,height=False)
 window.title("Age Calculator")
 
 # Heading and Subheading
-l1 = tk.Label(window,text="The Age Calculator!",font=("Arial", 20),fg="black",bg="#F7DC6F")
-l2 = tk.Label(window,font=("Arial",12),text="Enter your birthday which includes the day-month-year.",fg="black",bg="#F7DC6F")
+lb_heading = tk.Label(window,text="The Age Calculator!",font=("Arial", 20),fg="black",bg="#F7DC6F")
+lb_subheading = tk.Label(window,font=("Arial",12),text="Enter your birthday which includes the day-month-year.",fg="black",bg="#F7DC6F")
 
 # Date, Month and Year labels and entry
-l_d=tk.Label(window,text="Date: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-l_m=tk.Label(window,text="Month: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-l_y=tk.Label(window,text="Year: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-e1=tk.Entry(window,width=5)
-e2=tk.Entry(window,width=5)
-e3=tk.Entry(window,width=5)
+lb_date=tk.Label(window,text="Date: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+lb_month=tk.Label(window,text="Month: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+lb_year=tk.Label(window,text="Year: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+e_date=tk.Entry(window,width=5)
+e_month=tk.Entry(window,width=5)
+e_year=tk.Entry(window,width=5)
 
 # Calculate Age
-b1=tk.Button(window,text="Calculate Age!",font=("Arial",13),command=validation)
-l3 = tk.Label(window,text="The Calculated Age is: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-t1=tk.Text(window,width=3,height=0,state="disabled",bg="lightgreen",font=("Arial",24,"bold"))
+btn_calculate_age=tk.Button(window,text="Calculate Age!",font=("Arial",13),command=validation)
+lb_calculated_age = tk.Label(window,text="The Calculated Age is: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
+tbox_age=tk.Text(window,width=3,height=0,state="disabled",bg="lightgreen",font=("Arial",24,"bold"))
 
 # Exit 
-b2=tk.Button(window,text="Exit Application!",font=("Arial",13),command=exit)
+btn_exit=tk.Button(window,text="Exit Application!",font=("Arial",13),command=exit)
 
 # Placement
-l1.place(x=70,y=5)
-l2.place(x=10,y=40)
-l_d.place(x=60,y=80)
-l_m.place(x=60,y=105)
-l_y.place(x=60,y=130)
-e1.place(x=120,y=80)
-e2.place(x=120,y=105)
-e3.place(x=120,y=130)
-b1.place(x=30,y=170)
-l3.place(x=230,y=70)
-t1.place(x=300,y=100)
-b2.place(x=300,y=170)
+lb_heading.place(x=70,y=5)
+lb_subheading.place(x=10,y=40)
+lb_date.place(x=60,y=80)
+lb_month.place(x=60,y=105)
+lb_year.place(x=60,y=130)
+e_date.place(x=120,y=80)
+e_month.place(x=120,y=105)
+e_year.place(x=120,y=130)
+btn_calculate_age.place(x=30,y=170)
+lb_calculated_age.place(x=230,y=70)
+tbox_age.place(x=300,y=100)
+btn_exit.place(x=300,y=170)
 
 """
 Improvments:
